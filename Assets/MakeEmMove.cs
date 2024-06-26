e2aafdfef69950f9bfc2a8cf3d952c667d37f239
CIIT502PC26 <it@ciit.edu.ph>
2024-06-26 12:48:51 +0800

    2D Movement
    movement along coordinates TBA

---
+++ Assets/MakeEmMove.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MakeEmMove : MonoBehaviour
+{
+    public Animator Chicken;
+
+    void movement()
+    {
+        if (Input.GetKey(KeyCode.W)){
+            Chicken.SetBool("goUp", true);
+        }
+        else
+        {
+            Chicken.SetBool("goUp", false);
+        }
+        if (Input.GetKey(KeyCode.S)){
+            Chicken.SetBool("goDown", true);
+        }
+        else
+        {
+            Chicken.SetBool("goDown", false);
+        }
+        if (Input.GetKey(KeyCode.A)){
+            Chicken.SetBool("turnLeft", true);
+        }
+        else
+        {
+            Chicken.SetBool("turnLeft", false);
+        }
+        if (Input.GetKey(KeyCode.D)){
+            Chicken.SetBool("turnRight", true);
+        }
+        else
+        {
+            Chicken.SetBool("turnRight", false);
+        }
+    }
+
+    void Start()
+    {
+        Chicken = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        movement();
+    }
+}
